8b8388d94fb29edf404e3a1f35faec3c5ea1dfaa
steget <sthugo@student.chalmers.se>
2019-05-17 11:46:49 +0200

    La till fx:id på kategorier, start på hur man ska uppdatera listan med produkter när man byter kategori

--- src/iMatController.java
+++ src/iMatController.java
@@ -7,13 +7,17 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import se.chalmers.cse.dat216.project.*;
 
+import javax.swing.*;
+import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -54,6 +58,16 @@ public class iMatController implements Initializable, ShoppingCartListener {
     private TextField cvcField;
     @FXML
     private Label purchasesLabel;
+
+    //Category Pane
+    @FXML private TitledPane favoritesCategory;
+    @FXML private TitledPane breadCategory;
+    @FXML private TitledPane drinksCategory;
+    @FXML private TitledPane greensCategory;
+    @FXML private TitledPane meatAndFishCategory;
+    @FXML private TitledPane pantryCategory;
+    @FXML private TitledPane snacksCategory;
+
     
     // Other variables
     private final Model model = Model.getInstance();
@@ -93,6 +107,63 @@ public class iMatController implements Initializable, ShoppingCartListener {
     private void handleDoneAction(ActionEvent event) {
         closeAccountView();
     }
+
+    // Category pane actions
+    @FXML private void handleFavoriteSelectionAction(){
+        handleCategorySelection("favoritesCategory");
+    }
+    @FXML private void handleBreadSelectionAction(){
+        handleCategorySelection("breadCategory");
+    }
+    @FXML private void handleDrinksSelectionAction(){
+        handleCategorySelection("drinksCategory");
+    }
+    @FXML private void handleGreensSelectionAction(){
+        handleCategorySelection("greensCategory");
+    }
+    @FXML private void handleMeatAndFishSelectionAction(){
+        handleCategorySelection("meatAndFishCategory");
+    }
+    @FXML private void handlePantrySelectionAction(){
+        handleCategorySelection("pantryCategory");
+    }
+    @FXML private void handleSnacksSelectionAction(){
+        handleCategorySelection("snacksCategory");
+    }
+
+    /**
+     *
+     * See which category was pressed and update the product pane based on that
+     */
+    //todo Lägg till subkategorier och så att den faktiskt ändrar de som visas
+    private void handleCategorySelection(String id){
+        switch(id){
+            case ("favoritesCategory"): //set favorites to show
+                System.out.println("favo");
+                break;
+            case ("breadCategory"): //set bread to show
+                System.out.println("b");
+                break;
+            case ("drinksCategory"): //set drinks to show
+                System.out.println("d");
+                break;
+            case ("greensCategory"): //set greens to show
+                System.out.println("g");
+                break;
+            case ("meatAndFishCategory"): //set meatAndFish to show
+                System.out.println("maf");
+                break;
+            case ("pantryCategory"): //set pantry to show
+                System.out.println("p");
+                break;
+            case ("snacksCategory"): //set snacks to show
+                System.out.println("s");
+                break;
+            default:
+                System.out.println("no category matched :OOOOOOOO");
+                updateProductList(model.getProducts());
+        }
+    }
       
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -126,7 +197,8 @@ public class iMatController implements Initializable, ShoppingCartListener {
 
 
    private void updateProductCounts(){
-        //todo lägg till antal i varje ProductPanel som uppdateras här
+        //todo lägg till antal i varje ProductPanel som uppdateras här, kanske inte går att lösas
+       // utan får göra updateProductList istället
    }
     
     private void updateProductList(List<Product> products) {
