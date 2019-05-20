094c43c00135fa164663e99c70abb1300df7961a
Maatss <39332053+Maatss@users.noreply.github.com>
2019-05-20 15:06:48 +0200

    Fixed item removal from shoppingCart

--- src/Model.java
+++ src/Model.java
@@ -1,5 +1,4 @@
 /**
- *
  * @author oloft
  */
 
@@ -21,12 +20,12 @@ public class Model {
     private static Model instance = null;
     private IMatDataHandler iMatDataHandler;
 
-     private final ArrayList<String> availableCardTypes = new ArrayList<String>(Arrays.asList("MasterCard", "Visa"));
-     private final ArrayList<String> months = new ArrayList<String>(Arrays.asList("1", "2","3", "4", "5", "6"));
-     private final ArrayList<String> years = new ArrayList<String>(Arrays.asList("19", "20", "21", "22", "23", "24", "25"));
+    private final ArrayList<String> availableCardTypes = new ArrayList<String>(Arrays.asList("MasterCard", "Visa"));
+    private final ArrayList<String> months = new ArrayList<String>(Arrays.asList("1", "2", "3", "4", "5", "6"));
+    private final ArrayList<String> years = new ArrayList<String>(Arrays.asList("19", "20", "21", "22", "23", "24", "25"));
 
-     private boolean doShowHelpWizard = false;
-     private int helpWizardIndex = 0;
+    private boolean doShowHelpWizard = false;
+    private int helpWizardIndex = 0;
 
     /**
      * Constructor that should never be called, use getInstance() instead.
@@ -65,7 +64,7 @@ public class Model {
     public Product getProduct(int idNbr) {
         return iMatDataHandler.getProduct(idNbr);
     }
-    
+
     public List<Product> findProducts(String s) {
         return iMatDataHandler.findProducts(s);
     }
@@ -78,40 +77,42 @@ public class Model {
         return iMatDataHandler.getFXImage(p, width, height);
     }
 
-    public void addToShoppingCart(Product p) {
+    public void addToShoppingCart(Product p) { //todo some sort of feedback that the action succeeded/failed?
         ShoppingCart shoppingCart = iMatDataHandler.getShoppingCart();
 
         ShoppingItem item = new ShoppingItem(p);
         Model.getInstance().getShoppingCart().addItem(item);
-        
+
         //shoppingCart.addProduct(p);
     }
 
-    public void removeFromShoppingCart(Product p) {
-        ShoppingCart shoppingCart = iMatDataHandler.getShoppingCart();
+    public void removeFromShoppingCart(Product p) { //todo some sort of feedback that the action succeeded/failed?
+        ShoppingCart shoppingCart = Model.getInstance().getShoppingCart();
 
-        ShoppingItem item = new ShoppingItem(p);
-        Model.getInstance().getShoppingCart().addItem(item);
-
-        //shoppingCart.addProduct(p);
+        for (ShoppingItem cartItem : shoppingCart.getItems()) {
+            if(cartItem.getProduct().getProductId() == p.getProductId()) {
+                shoppingCart.removeItem(cartItem);
+                break;
+            }
+        }
     }
 
     public List<String> getCardTypes() {
         return availableCardTypes;
     }
-    
+
     public List<String> getMonths() {
         return months;
     }
-    
+
     public List<String> getYears() {
         return years;
     }
-    
+
     public CreditCard getCreditCard() {
         return iMatDataHandler.getCreditCard();
     }
-    
+
     public Customer getCustomer() {
         return iMatDataHandler.getCustomer();
     }
@@ -136,15 +137,15 @@ public class Model {
         iMatDataHandler.shutDown();
     }
 
-    public void toggleFavorite(Product p){
+    public void toggleFavorite(Product p) {
         if (iMatDataHandler.isFavorite(p)) {
             iMatDataHandler.removeFavorite(p);
-        }else{
+        } else {
             iMatDataHandler.addFavorite(p);
         }
     }
 
-    public boolean checkIfFavorite(Product p){
+    public boolean checkIfFavorite(Product p) {
         return iMatDataHandler.isFavorite(p);
     }
 
