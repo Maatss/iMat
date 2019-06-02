8e2413f860ed3792a3075fecb447be9dbd9a153b
Steget <sthugo@student.chalmers.se>
2019-06-02 17:55:22 +0200

    Show text related to favorites when there is no favorites

--- src/iMatController.java
+++ src/iMatController.java
@@ -77,6 +77,10 @@ public class iMatController implements Initializable, ShoppingCartListener {
     @FXML
     private Label productsNoResultsBottomLabel;
     @FXML
+    private Label productsNoFavoritesTopLabel;
+    @FXML
+    private Label productsNoFavoritesBottomLabel;
+    @FXML
     private Button searchButton;
 
     @FXML
@@ -401,6 +405,8 @@ public class iMatController implements Initializable, ShoppingCartListener {
         } else {
             productsNoResultsTopLabel.setVisible(true);
             productsNoResultsBottomLabel.setVisible(true);
+            productsNoFavoritesTopLabel.setVisible(false);
+            productsNoFavoritesBottomLabel.setVisible(false);
         }
     }
 
@@ -599,6 +605,12 @@ public class iMatController implements Initializable, ShoppingCartListener {
         } else if (category.equals("favoritesCategory")) {
             updateProductList(model.getFavoriteProducts());
             showProductsPane(); //Basically resets resultscreen
+            if(model.getFavoriteProducts().isEmpty()){ //ugly fix to say no favorites have been added
+                productsNoResultsTopLabel.setVisible(false);
+                productsNoResultsBottomLabel.setVisible(false);
+                productsNoFavoritesTopLabel.setVisible(true);
+                productsNoFavoritesBottomLabel.setVisible(true);
+            }
         } else if (pc != null) {
             updateProductList(model.getCategoryProducts(pc));
             showProductsPane(); //Basically resets resultscreen
@@ -826,20 +838,6 @@ public class iMatController implements Initializable, ShoppingCartListener {
         categoryLabel.setText("Tidigare köp");
         updatePrevOrdersList(expandLatest);
         previousOrdersPane.toFront();
-    }
-
-    @FXML
-    private void handleReturnToCartHoverEnter() {
-        returnToCartImageView.setImage(new Image("images/shoppingCartBackDark.png"));
-    }
-
-    @FXML
-    private void handleReturnToCartHoverExit() {
-        returnToCartImageView.setImage(new Image("images/shoppingCartBack.png"));
-    }
-
-    private void closeReceiptsView() {
-        //todo / other branch
     }
 
     // Shop pane methods
